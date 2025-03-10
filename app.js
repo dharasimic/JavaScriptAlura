67d51fd87a9b17578c35e92c1ac9bc97c64b2183
Dhara Simic <dhara.soares@modalgr.io>
2025-03-10 10:20:59 -0300

    feat: adicionado um laço de repetição e a contagem de tentativas

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert('Número Secreto');
 
-let numeroSecreto = 1;
-let numeroChute = prompt('Escolha um número entre 1 e 10.');
+let numeroSecreto = 5;
+let numeroChute;
+let tentativas = 1;
 
-if (numeroSecreto == numeroChute) {
-    alert(`Parabéns, você acertou o número secreto ${numeroSecreto}`);
-}
+while (numeroChute != numeroSecreto) {
+    numeroChute = prompt('Escolha um número entre 1 e 10.');
 
-else {
-    alert('Que pena, você errou!');
+    if (numeroChute == numeroSecreto) {
+        alert(`Parabéns, você acertou o número secreto ${numeroSecreto} em ${tentativas} tentativas.`);
+    }
+
+    else {
+        if (numeroSecreto > numeroChute) {
+            alert(`O número secreto é maior que ${numeroChute}.`);
+        }
+        else {
+            alert(`O número secreto é menor que ${numeroChute}.`);
+        }
+        tentativas++;
+    }
 }
